49eb60145ee33837cd97856c45a14d24a17665bc
princychristal <41038952+princychristal@users.noreply.github.com>
2018-09-26 15:33:29 +0530

    Create 18.py

---
+++ 18.py
@@ -0,0 +1,13 @@
+y=int(input("begin="))
+z=int(input("end="))
+for x in range(y+1,z):
+    a = 0
+    b = 0
+    t=x
+    while x>0:
+            re=x%10
+            a=re**3
+            a=a+b
+            x = x // 10
+    if(t==s):
+         print(s)
