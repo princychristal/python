e91765b4f216d95bb22e142111ae6116c02cbaf8
princychristal <41038952+princychristal@users.noreply.github.com>
2018-09-26 14:43:40 +0530

    Create alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,8 @@
+y="a"
+v=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+for x in v:
+    if(x==y):
+        print("Alphabet")
+        break
+    else:
+        print("no")
