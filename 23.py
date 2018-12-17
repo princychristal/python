41b20804fcfc5101a2a369454850d74867e13d3b
princychristal <41038952+princychristal@users.noreply.github.com>
2018-12-17 10:21:15 +0530

    Create 23.py

---
+++ 23.py
@@ -0,0 +1,10 @@
+print("print minimum element")
+from array import*
+n=int(input("N="))
+for y in range(1,n-1);
+   my=array('i',[y])
+m=my[0]
+for x in my:
+   if(x<=my[0]):
+       m=x
+print("min",m)
