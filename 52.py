7194cde9c3565103a67c6e26f6d2b2e411efd6b5
princychristal <41038952+princychristal@users.noreply.github.com>
2018-12-28 12:12:20 +0530

    Create 52.py

---
+++ 52.py
@@ -0,0 +1,8 @@
+def main():
+        try:
+                x=['zero','one','two','three','four','five','six','seven','eight','nine','ten']
+                y=int(input())
+                print(x[y])
+        except:
+                print('invalid');
+main()
