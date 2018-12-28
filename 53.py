6bb041fdea3752ead61702edee282804a5fb0759
princychristal <41038952+princychristal@users.noreply.github.com>
2018-12-28 12:15:37 +0530

    Create 53.py

---
+++ 53.py
@@ -0,0 +1,10 @@
+try:
+        x=int(input())
+        sum=0
+        while(x!=0):
+                rem=x%10
+                sum+=rem
+                x=x//10
+        print(sum);
+except:
+        print('invalid');
