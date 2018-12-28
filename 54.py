30c910f4810da5a94bbe47e7be8f5d61dac4693b
princychristal <41038952+princychristal@users.noreply.github.com>
2018-12-28 12:18:27 +0530

    Create 54.py

---
+++ 54.py
@@ -0,0 +1,9 @@
+def main():
+        try:
+                x=int(input())
+                if x%2!=0:
+                        x-=1
+                print(x);
+        except:
+                print('invalid');
+main()
