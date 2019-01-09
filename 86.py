7d7547abd89eff5f44478513728645eebc172803
princychristal <41038952+princychristal@users.noreply.github.com>
2019-01-09 01:04:28 -0800

    Create 86.py

---
+++ 86.py
@@ -0,0 +1,15 @@
+def gram():
+	a=input()
+	b=list(a)
+	c=[]
+	for i in b:
+		if not x in c:
+			r.append(x)
+	if len(b)==len(c):
+		print('yes');
+	else :
+		print('no');
+try:
+	gram()
+except:
+	print('invalid');
